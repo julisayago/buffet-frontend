e80c943041036b4c693b853a2920b70d7b5e82bd
Julieta <julysayago12@gmail.com>
2025-10-14 11:10:17 -0300

    feat: ocultar Navbar y Footer en las vistas de login y registro

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Login from "@pages/login";
-import Home from "@pages/home";
-import Register from "@pages/register";
-import Contacto from "@pages/contacto";
-import Productos from "@pages/productos";
-import ProductoDetalle from "@pages/producto-detalle";
-import Carrito from "@pages/carrito";
-import Pedidos from "@pages/pedidos";
-import Detalle from "@pages/detalle";
-import QRPage from "@pages/qr-page";
-import Perfil from "@pages/perfil";
+import Home from "@pages/user/home";
+import Register from "@pages/user/register";
+import Contacto from "@pages/user/contacto";
+import Productos from "@pages/user/productos";
+import ProductoDetalle from "@pages/user/producto-detalle";
+import Carrito from "@pages/user/carrito";
+import Pedidos from "@pages/user/pedidos";
+import Detalle from "@pages/user/detalle";
+import QRPage from "@pages/user/qr-page";
+import Perfil from "@pages/user/perfil";
 import './app.css';
-import Navbar from '@components/navbar.jsx';
-import Footer from '@components/footer.jsx';
+import Navbar from '@components/navbar/navbar.jsx';
+import Footer from '@components/footer/footer.jsx';
 
-function App() {
+function AppLayout() {
+  const location = useLocation();
+
+  const hideNavFooter = location.pathname === "/" || location.pathname === "/register";
+
   return (
-    <BrowserRouter>
-      <Navbar />
+    <>
+      {!hideNavFooter && <Navbar />}
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
@@ -31,10 +35,19 @@ function App() {
         <Route path="/qr/:id" element={<QRPage />} />
         <Route path="/perfil" element={<Perfil />} />
       </Routes>
-      <Footer />
+      {!hideNavFooter && <Footer />}
+    </>
+  );
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <AppLayout />
     </BrowserRouter>
   );
 }
 
 export default App;
+
 
