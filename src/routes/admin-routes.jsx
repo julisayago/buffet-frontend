1d941c919b6e10adcc999096778d0d0203fd6a56
Julieta <julysayago12@gmail.com>
2025-10-28 21:21:19 -0300

    feat: configura rutas iniciales del proyecto

---
+++ src/routes/admin-routes.jsx
@@ -0,0 +1,30 @@
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "@adminpages/dashboard/dashboard";
+import QRAdmin from "@adminpages/qr/qr-admin";
+import ProductosAdmin from "@adminpages/productos/productos-admin";
+import EditarProducto from "@adminpages/productos/producto-editar";
+import CrearProducto from "@adminpages/productos/producto-crear";
+import UsuariosAdmin from "@adminpages/usuarios/usuarios-admin";
+import EditarUsuario from "@adminpages/usuarios/usuario-editar";
+import CrearUsuario from "@adminpages/usuarios/usuario-crear";
+import PedidosAdmin from "@adminpages/pedidos/pedidos-admin";
+import EditarPedido from "@adminpages/pedidos/pedido-editar";
+import CrearPedido from "@adminpages/pedidos/pedido-crear";
+
+export default function AdminRoutes() {
+  return (
+    <Routes>
+      <Route path="" element={<Dashboard />} />
+      <Route path="qr" element={<QRAdmin />} />
+      <Route path="productos" element={<ProductosAdmin />} />
+      <Route path="productos/agregar" element={<CrearProducto />} />
+      <Route path="productos/editar/:id" element={<EditarProducto />} />
+      <Route path="usuarios" element={<UsuariosAdmin />} />
+      <Route path="usuarios/agregar" element={<CrearUsuario />} />
+      <Route path="usuarios/editar/:id" element={<EditarUsuario />} />
+      <Route path="pedidos" element={<PedidosAdmin />} />
+      <Route path="pedidos/agregar" element={<CrearPedido />} />
+      <Route path="pedidos/editar/:id" element={<EditarPedido />} />
+    </Routes>
+  );
+}
---
+++ src/routes/user-routes.jsx
@@ -0,0 +1,27 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "@userpages/home/home";
+import Register from "@userpages/registro/registro";
+import Contacto from "@userpages/contacto/contacto";
+import Productos from "@userpages/producto/productos";
+import ProductoDetalle from "@userpages/producto/producto-detalle";
+import Carrito from "@userpages/carrito/carrito";
+import Pedidos from "@userpages/pedidos/pedidos";
+import Detalle from "@userpages/pedidos/detalle-pedido";
+import QRPage from "@userpages/qr/qr-page";
+import Perfil from "@userpages/perfil/perfil";
+
+export default function UserRoutes() {
+  return (
+    <Routes>
+      <Route path="home" element={<Home />} />
+      <Route path="contacto" element={<Contacto />} />
+      <Route path="perfil" element={<Perfil />} />
+      <Route path="productos" element={<Productos />} />
+      <Route path="producto/:id" element={<ProductoDetalle />} />
+      <Route path="carrito" element={<Carrito />} />
+      <Route path="pedidos" element={<Pedidos />} />
+      <Route path="detalle/:id" element={<DetallePedido />} />
+      <Route path="qr/:id" element={<QRPage />} />
+    </Routes>
+  );
+}
