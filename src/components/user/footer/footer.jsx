3f9e7f256314813c01b89f3044888b0ac3d6d0d9
Rocio Maldonado <138414271+RocioMaldonado@users.noreply.github.com>
2025-11-21 01:42:18 -0300

    style: agregar contenido adicional al footer

--- src/components/user/footer/footer.jsx
+++ src/components/user/footer/footer.jsx
@@ -4,7 +4,13 @@ import "./footer.css";
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>© 2025 Buffet UNaB. All rights reserved.</p>
+      <div className="footer-left">
+        <p>Universidad Nacional Guillermo Brown</p>
+        <p>Blas Parera 132, Burzaco</p>
+      </div>
+      <div className="footer-right">
+          <p>© 2025 Buffet UNaB. All rights reserved.</p>
+        </div>
     </footer>
   );
 };
