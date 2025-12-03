70fddd46bd1dc9fbe3ccf2ee1a74a25c77ac0e7b
Julieta <julysayago12@gmail.com>
2025-12-02 22:58:18 -0300

    feat (qr user): conectar front con back

--- src/pages/user/qr/qr-page.jsx
+++ src/pages/user/qr/qr-page.jsx
@@ -1,42 +1,83 @@
 import { useParams, useNavigate } from "react-router-dom";
-import { QRCodeCanvas } from "qrcode.react";
 import "./qr-page.css";
 import { AiOutlineArrowLeft } from "react-icons/ai";
+import { useState, useEffect } from "react";
+import Loader from "@components/loader/loader";
+import { API_URL } from "@config/api";
 
 function QRPage() {
   const { id } = useParams();
   const navigate = useNavigate();
 
-  return (
-    <>
-      <div className="qr-container">
-        <div className="qr-card">
-          {/* Encabezado con flecha */}
-          <div className="qr-header">
-            <button
-              className="back-button"
-              type="button"
-              onClick={() => navigate(-1)}
-            >
-              <AiOutlineArrowLeft size={20} />
-            </button>
-            <h2 className="qr-titulo">Código QR</h2>
-          </div>
+  const [loading, setLoading] = useState(true);
+  const [qrCode, setQrCode] = useState(null);
+  const [numeroPedido, setNumeroPedido] = useState("");
 
-          {/* Contenido del QR */}
-          <div className="qr-content">
-            <p>
-              Este es el código QR para tu pedido <strong>#{id}</strong>
-            </p>
-            <QRCodeCanvas value={`Pedido-${id}`} size={220} />
-          </div>
+  useEffect(() => {
+    const fetchQR = async () => {
+      try {
+        const token = localStorage.getItem("token");
+
+        const response = await fetch(`${API_URL}/orders/${id}`, {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        const data = await response.json();
+
+        console.log("QR recibido:", data.qr_code);
+
+        if (data.qr_code) {
+          setQrCode(data.qr_code);
+          setNumeroPedido(data.order.numero_pedido);
+        }
+
+      } catch (error) {
+        console.error("Error al cargar QR:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchQR();
+  }, [id]);
+
+  if (loading) {
+    return <Loader text="Cargando código QR..." />;
+  }
+
+  return (
+    <div className="qr-container">
+      <div className="qr-card">
+
+        <div className="qr-header">
+          <button
+            className="back-button"
+            type="button"
+            onClick={() => navigate(-1)}
+          >
+            <AiOutlineArrowLeft size={20} />
+          </button>
+          <h2 className="qr-titulo">Código QR</h2>
         </div>
+
+        <div className="qr-content">
+          <p>
+            Este es el código QR para tu pedido <strong>#{numeroPedido}</strong>
+          </p>
+
+          {qrCode ? (
+            <img src={qrCode} alt="QR"/>
+          ) : (
+            <p>No se pudo cargar el QR.</p>
+          )}
+        </div>
+
       </div>
-    </>
+    </div>
   );
 }
 
 export default QRPage;
-
-
-
